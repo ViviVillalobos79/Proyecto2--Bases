41fbdfb62c1240709ba7348633a07ec5e4696914
ViviVillalobos79 <vvillalobosv@estudiantec.cr>
2020-11-29 13:11:56 -0600

    gestion de documentos profesor

--- FRONTEND/src/app/pages/ges-docos-profe/ges-docos-profe.component.ts
+++ FRONTEND/src/app/pages/ges-docos-profe/ges-docos-profe.component.ts
@@ -13,85 +13,7 @@ export class GesDocosProfeComponent implements OnInit {
   username: string;
   nombrecurso:string;
   grupo:string;
-
-
-  informaciones = [
-    {
-      Nombre: 'Viviana',
-      Descripcion: 'Cartago, Cartago, Costa Rica',
-      tipo: 'atleta',
-      id: '0',
-    },
-    {
-      Nombre: 'Sebastian',
-      Descripcion: 'La Garriga, CT, Spain',
-      tipo: 'atleta',
-      id: '1',
-    },
-    { Nombre: 'Fernanda', Descripcion: 'Itanhaem, SP', tipo: 'atleta', id: 2 },
-    {
-      Nombre: 'Manuel',
-      Descripcion: 'Cartago, Cartago, Costa Rica',
-      tipo: 'atleta',
-      id: '3',
-    },
-    {
-      Nombre: 'Ana',
-      Descripcion: 'San Jose, San Jose, Costa Rica',
-      tipo: 'atleta',
-      id: '4',
-    },
-    {
-      Nombre: 'Ciclitas',
-      Descripcion: 'Somos ciclistas muy cools',
-      tipo: 'grupo',
-      id: '5',
-    },
-    {
-      Nombre: 'Corredores',
-      Descripcion: 'Corredores que nos gusta hablar del deporte',
-      tipo: 'grupo',
-      id: '6',
-    },
-    {
-      Nombre: 'Kayak Costa Rica',
-      Descripcion: 'Deportistas de Costa Rica aficionados al Kayak',
-      tipo: 'grupo',
-      id: '7',
-    },
-    {
-      Nombre: 'Nadadores profesionales',
-      Descripcion: 'Nadadores profesionales de la maraton',
-      tipo: 'grupo',
-      id: '8',
-    },
-    {
-      Nombre: 'Maraton',
-      Descripcion: 'Maraton mas importante de Costa Rica',
-      tipo: 'competencia',
-      id: '9',
-    },
-    {
-      Nombre: '4 piscinas',
-      Descripcion: '4 piscinas olimpicas',
-      tipo: 'competencia',
-      id: '10',
-    },
-    {
-      Nombre: 'Milla',
-      Descripcion: 'Debe hacer una milla para completar el reto',
-      tipo: 'reto',
-      id: '11',
-    },
-    {
-      Nombre: '4km de kayak',
-      Descripcion: '4km de kayak para completar el reto',
-      tipo: 'reto',
-      id: '12',
-    },
-  ];
-
-  informacionA = [];
+  nombreCarpeta:string;
 
   carpetas = ["Presentaciones","Quices","Exámenes","Proyectos"]
 
@@ -109,6 +31,14 @@ export class GesDocosProfeComponent implements OnInit {
 
 
   buscar() {
+  }
+
+  crearCarpeta(nombre){
+    this.nombreCarpeta = nombre;
+  }
+
+  visitarCarpeta(nombre){
+    //Visitar la carpeta
   }
 
 
