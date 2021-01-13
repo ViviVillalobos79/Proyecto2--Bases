e007dc5a241711d7fc563c5e5030ccc1edf3eb5b
ViviVillalobos79 <vvillalobosv@estudiantec.cr>
2021-01-12 22:57:27 -0600

    verificacion del login

--- FRONTEND/src/app/pages/login/login.component.ts
+++ FRONTEND/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Persona } from 'src/app/models/Persona';
 import { PersonaService } from '../../services/mongo/persona.service';
 
 @Component({
@@ -12,6 +13,8 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
   rol: string;
+  loged:boolean;
+  personas:Persona[];
 
   constructor(
     private personaSvc: PersonaService,
@@ -20,14 +23,11 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.loged = false;
     this.personaSvc.getAll().subscribe((res) => {
       console.log('Res ', res);
+      this.personas = res;
     });
-  }
-
-  login() {
-    //Verifica las credenciales
-    this.router.navigate(['inicio', this.username]);
   }
 
   rolSet(rol) {
@@ -41,5 +41,23 @@ export class LoginComponent implements OnInit {
 
   setPassword(password) {
     this.password = password;
+  }
+
+  checkCredentials(){
+    console.log("hola");
+    this.personas.forEach(element => {
+
+      if(this.rol == element.tipo_Persona && this.username == element.identificador.toString() && this.password == element.clave){
+        this.loged = true;
+      }
+
+    });
+  }
+
+  login() {
+    //Verifica las credenciales
+    this.checkCredentials();
+    if(this.loged){
+      this.router.navigate(['inicio', this.username]);}
   }
 }
