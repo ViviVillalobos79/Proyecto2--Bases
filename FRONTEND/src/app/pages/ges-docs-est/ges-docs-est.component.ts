4527e9e3f54b2886889fd034e402d24ed8cfd3c8
ViviVillalobos79 <vvillalobosv@estudiantec.cr>
2021-01-12 22:34:57 -0600

    routing cambios

--- FRONTEND/src/app/pages/ges-docs-est/ges-docs-est.component.ts
+++ FRONTEND/src/app/pages/ges-docs-est/ges-docs-est.component.ts
@@ -1,36 +1,34 @@
 import { Output } from '@angular/core';
-import { Component,EventEmitter, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-ges-docs-est',
   templateUrl: './ges-docs-est.component.html',
-  styleUrls: ['./ges-docs-est.component.css']
+  styleUrls: ['./ges-docs-est.component.css'],
 })
 export class GesDocsEstComponent implements OnInit {
-
-  carnet:string;
-  nombrecurso:string;
-  grupo:string;
-  idCurso:string;
+  carnet: string;
+  nombrecurso: string;
+  grupo: string;
+  idCurso: string;
 
   carpetas = [
-    {nombre:"Presentaciones", id: 0},
-    {nombre:"Quices", id: 1},
-    {nombre:"Quices", id: 2},
-    {nombre:"Quices", id: 3}
+    { nombre: 'Presentaciones', id: 0 },
+    { nombre: 'Quices', id: 1 },
+    { nombre: 'Quices', id: 2 },
+    { nombre: 'Quices', id: 3 },
   ];
 
   //carpetas = ["Presentaciones","Quices","Quices","Quices"]
 
-  constructor(private router: Router,
-    private _route: ActivatedRoute) {
-      this.carnet = this._route.snapshot.paramMap.get('carnet');
-      this.idCurso = this._route.snapshot.paramMap.get('idCurso');
-    }
+  constructor(private router: Router, private _route: ActivatedRoute) {
+    this.carnet = this._route.snapshot.paramMap.get('carnet');
+    this.idCurso = this._route.snapshot.paramMap.get('idCurso');
+  }
 
   ngOnInit(): void {
-    this.nombrecurso = "LABORATORIO DE CIRCUITOS ELECTRICOS";
-    this.grupo = "4";
+    this.nombrecurso = 'LABORATORIO DE CIRCUITOS ELECTRICOS';
+    this.grupo = '4';
     //Verificar las carpetas del curso preguntandole al server
     //Solicitar el nombre del curso y el grupo para rellenar la informacion
   }
@@ -46,13 +44,17 @@ export class GesDocsEstComponent implements OnInit {
     this.router.navigate(['inicio', this.carnet]);
   }
 
-  noticiasGo(){
+  noticiasGo() {
     this.router.navigate(['noticias', this.carnet, this.idCurso]);
   }
+  documentosGo(){
+    this.router.navigate(['gesDocEst', this.carnet, this.idCurso]);
+  }
+  evaluacionesGo(){
 
-
-  abrir() {
-    this.router.navigate(['docEst']);
   }
 
+  abrir(id) {
+    this.router.navigate(['docEst']);
+  }
 }
--- FRONTEND/src/app/pages/noticias/noticias.component.ts
+++ FRONTEND/src/app/pages/noticias/noticias.component.ts
@@ -1,33 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
-  styleUrls: ['./noticias.component.css']
+  styleUrls: ['./noticias.component.css'],
 })
 export class NoticiasComponent implements OnInit {
-  username:string;
-  nombrecurso:string;
-  grupo:string;
-  noticias=[{}];
-  
+  username: string;
+  nombrecurso: string;
+  grupo: string;
+  noticias = [{}];
+  idCurso:string;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.nombrecurso = "LABORATORIO DE CIRCUITOS ELECTRICOS";
-    this.grupo = "4";
-    this.noticias=[
-      {fecha:"12 de Diciembre",
-      titulo:"Notas",
-      cuerpo:"Las notas estan entregadas, se pueden ver en evaluaciones",
-      autor:"Diego"
-    },
-      {fecha:"15 de Diciembre",
-      titulo:"Quiz",
-      cuerpo:"Descargar el quiz de documentos/quices, para la clase de hoy",
-      autor:"Isaac"
-    }]
+  constructor(private router: Router, private _route: ActivatedRoute) {
+    this.username = this._route.snapshot.paramMap.get('carnet');
+    this.idCurso = this._route.snapshot.paramMap.get('idCurso');
   }
 
+  ngOnInit(): void {
+    this.nombrecurso = 'LABORATORIO DE CIRCUITOS ELECTRICOS';
+    this.grupo = '4';
+    this.noticias = [
+      {
+        fecha: '12 de Diciembre',
+        titulo: 'Notas',
+        cuerpo: 'Las notas estan entregadas, se pueden ver en evaluaciones',
+        autor: 'Diego',
+      },
+      {
+        fecha: '15 de Diciembre',
+        titulo: 'Quiz',
+        cuerpo: 'Descargar el quiz de documentos/quices, para la clase de hoy',
+        autor: 'Isaac',
+      },
+    ];
+  }
 }
