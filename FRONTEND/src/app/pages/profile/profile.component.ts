9f736b671ec595536b905245d892772211185767
ViviVillalobos79 <vvillalobosv@estudiantec.cr>
2021-01-13 21:03:10 -0600

    Revert "profile"
    This reverts commit 4db56fba0c109ad52b8520006f5e2dcdb7810906.

--- FRONTEND/src/app/pages/profile/profile.component.ts
+++ FRONTEND/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Output } from '@angular/core';
 import { Component, EventEmitter, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Persona } from 'src/app/models/Persona';
 import { PersonaService } from '../../services/mongo/persona.service';
 
@@ -11,55 +11,30 @@ import { PersonaService } from '../../services/mongo/persona.service';
   providers: [PersonaService],
 })
 export class ProfileComponent implements OnInit {
-  carne: string;
+  carne: number;
   nombre: string;
   correo: string;
   telefono: number;
   contraseña: string;
-  personas: Persona[];
-
-  estudiante:Persona;
+  personas:Persona[];
 
   @Output() onChange: EventEmitter<File> = new EventEmitter<File>();
-  constructor(
-    private router: Router,
-    private personaSvc: PersonaService,
-    private _route: ActivatedRoute
-  ) {
-    this.carne = this._route.snapshot.paramMap.get('cedula');
-  }
+  constructor(private router: Router, private personaSvc: PersonaService) {}
 
   ngOnInit(): void {
-    //this.carne = 2017147709;
-    //this.nombre = 'Lucy';
-    //this.correo = 'lmg@gmail.com';
+    this.carne = 2017147709;
+    this.nombre = 'Lucy';
+    this.correo = 'lmg@gmail.com';
     this.telefono = 89317701;
-    //this.contraseña = 'Lucy';
+    this.contraseña = 'Lucy';
 
     this.personaSvc.getAll().subscribe((res) => {
       this.personas = res;
       console.log(this.personas);
-      this.hola(this.personas);
-    });
-
-    this.personaSvc.getById(2015053251).subscribe((res) => {
-      this.estudiante = res[0];
-      console.log(res[0].apellido1);
-
     });
   }
 
   perfilGo() {
     this.router.navigate(['profile', this.carne]);
-  }
-
-  hola(per: Persona[]) {
-    per.forEach((element) => {
-      if (element.identificador.toString() == this.carne) {
-        this.nombre = element.primer_Nombre + ' ' + element.apellido1;
-        this.correo = element.correo;
-        this.contraseña = element.clave;
-      }
-    });
   }
 }
